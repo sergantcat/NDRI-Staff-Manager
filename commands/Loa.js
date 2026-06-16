993a5bd3dad4e5463d161cd546f5a5776166c7d0
sergantcat <sergantcat@gmail.com>
2026-06-16 22:02:09 +0200

    2nd NDRI Bot

--- commands/Loa.js
+++ commands/Loa.js
@@ -1,6 +1,17 @@
-const {SlashCommandBuilder} = require('discord.js')
+const {SlashCommandBuilder,EmbedBuilder} = require('discord.js')
 
-module.exports  {
-   .setName = "h"
+module.exports = {
+   Data: NewSlashCommandBuilder()
+   .setName('LoaRequest')
+   .setDescription('[DEV] Request Leave of abscense')
+   .addUserOption(option =>
+    option.setName('user')
+        .setDescription('Выберите пользователя')
+        .setRequired(true)
+)
+.addStringOption(option =>
+    option.setName('loa_time')
+        .setDescription('Укажите время отсутствия (например: 3 дня, до понедельника)')
+        .setRequired(true)
+),
 }
-
